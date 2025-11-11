73f98740c14d6802de0aa6bbeea5961185e604a9
vladforest08vml <vladforest08vml@gmail.com>
2025-11-11 18:16:43 +0200

    global refactoring

--- CPP-client/Yaderka/program.cpp
+++ CPP-client/Yaderka/program.cpp
@@ -82,8 +82,6 @@ int main()
 
 		 }
 	 } while (valid_choise);
-	 
-
  }
 
  void clear()
@@ -106,7 +104,7 @@ int main()
 	 sockaddr_in server;
 
 	 server.sin_family = AF_INET;
-	 server.sin_port = htons(8686);                   // port
+	 server.sin_port = htons(8686);                       // port
 	 server.sin_addr.s_addr = inet_addr("192.168.1.77"); // IP 
 
 	 connect(id, (sockaddr*)&server, sizeof(server));
